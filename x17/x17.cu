828cdd69a77d3b0697bd16e2dcd912951905b58f
Chris <34682781+monkins1010@users.noreply.github.com>
2018-11-18 13:57:28 +0000

    added minato

--- x17/x17.cu
+++ x17/x17.cu
@@ -24,6 +24,7 @@ extern "C" {
 
 #include "sph/sph_sha2.h"
 #include "sph/sph_haval.h"
+#include "sph/sph_streebog.h"
 }
 
 #include "miner.h"
@@ -31,6 +32,7 @@ extern "C" {
 #include "x11/cuda_x11.h"
 
 static uint32_t *d_hash[MAX_GPUS];
+static uint32_t *d2_hash[MAX_GPUS];
 
 extern void x16_echo512_cpu_hash_64(int thr_id, uint32_t threads, uint32_t *d_hash);
 
@@ -49,107 +51,112 @@ extern void x15_whirlpool_cpu_hash_64(int thr_id, uint32_t threads, uint32_t sta
 extern void x15_whirlpool_cpu_free(int thr_id);
 
 extern void x17_sha512_cpu_init(int thr_id, uint32_t threads);
-extern void x17_sha512_cpu_hash_64(int thr_id, uint32_t threads, uint32_t startNounce, uint32_t *d_hash);
+extern void x17_sha512_cpu_hash_64(int thr_id, uint32_t threads, uint32_t startNounce, uint32_t *d_hash, uint32_t *d2_hash);
 
 extern void x17_haval256_cpu_init(int thr_id, uint32_t threads);
-extern void x17_haval256_cpu_hash_64(int thr_id, uint32_t threads, uint32_t startNounce, uint32_t *d_hash, const int outlen);
+extern void x17_haval256_cpu_hash_64(int thr_id, uint32_t threads, uint32_t startNounce, uint32_t *d_hash, const int outlen, uint32_t *d2_hash);
 
+extern void streebog_cpu_hash_64(int thr_id, uint32_t threads, uint32_t *d_hash);
+extern void streebog_hash_64_maxwell(int thr_id, uint32_t threads, uint32_t *d_hash);
 
 // X17 CPU Hash (Validation)
-extern "C" void x17hash(void *output, const void *input)
+extern "C" void x18hash(void *output, const void *input)
 {
-	unsigned char _ALIGN(64) hash[128];
+	sph_blake512_context      ctx_blake;
+	sph_bmw512_context        ctx_bmw;
+	sph_groestl512_context    ctx_groestl;
+	sph_jh512_context         ctx_jh;
+	sph_keccak512_context     ctx_keccak;
+	sph_skein512_context      ctx_skein;
+	sph_luffa512_context      ctx_luffa;
+	sph_cubehash512_context   ctx_cubehash;
+	sph_shavite512_context    ctx_shavite;
+	sph_simd512_context       ctx_simd;
+	sph_echo512_context       ctx_echo;
+	sph_hamsi512_context      ctx_hamsi;
+	sph_fugue512_context      ctx_fugue;
+	sph_shabal512_context     ctx_shabal;
+	sph_whirlpool_context     ctx_whirlpool;
+	sph_sha512_context        ctx_sha2;
+	sph_haval256_5_context    ctx_haval;
+	sph_gost512_context       ctx_gost;
 
-	// x11 + hamsi12-fugue13-shabal14-whirlpool15-sha512-haval256
-
-	sph_blake512_context ctx_blake;
-	sph_bmw512_context ctx_bmw;
-	sph_groestl512_context ctx_groestl;
-	sph_jh512_context ctx_jh;
-	sph_keccak512_context ctx_keccak;
-	sph_skein512_context ctx_skein;
-	sph_luffa512_context ctx_luffa;
-	sph_cubehash512_context ctx_cubehash;
-	sph_shavite512_context ctx_shavite;
-	sph_simd512_context ctx_simd;
-	sph_echo512_context ctx_echo;
-	sph_hamsi512_context ctx_hamsi;
-	sph_fugue512_context ctx_fugue;
-	sph_shabal512_context ctx_shabal;
-	sph_whirlpool_context ctx_whirlpool;
-	sph_sha512_context ctx_sha512;
-	sph_haval256_5_context ctx_haval;
-
+	//these uint512 in the c++ source of the client are backed by an array of uint32
+	uint32_t _ALIGN(64) hashA[16], hashB[16];
 
 	sph_blake512_init(&ctx_blake);
 	sph_blake512(&ctx_blake, input, 80);
-	sph_blake512_close(&ctx_blake, hash);
+	sph_blake512_close(&ctx_blake, hashA);
 
 	sph_bmw512_init(&ctx_bmw);
-	sph_bmw512(&ctx_bmw, (const void*) hash, 64);
-	sph_bmw512_close(&ctx_bmw, hash);
+	sph_bmw512(&ctx_bmw, hashA, 64);
+	sph_bmw512_close(&ctx_bmw, hashB);
 
 	sph_groestl512_init(&ctx_groestl);
-	sph_groestl512(&ctx_groestl, (const void*) hash, 64);
-	sph_groestl512_close(&ctx_groestl, hash);
+	sph_groestl512(&ctx_groestl, hashB, 64);
+	sph_groestl512_close(&ctx_groestl, hashA);
 
 	sph_skein512_init(&ctx_skein);
-	sph_skein512(&ctx_skein, (const void*) hash, 64);
-	sph_skein512_close(&ctx_skein, hash);
+	sph_skein512(&ctx_skein, hashA, 64);
+	sph_skein512_close(&ctx_skein, hashB);
 
 	sph_jh512_init(&ctx_jh);
-	sph_jh512(&ctx_jh, (const void*) hash, 64);
-	sph_jh512_close(&ctx_jh, hash);
+	sph_jh512(&ctx_jh, hashB, 64);
+	sph_jh512_close(&ctx_jh, hashA);
 
 	sph_keccak512_init(&ctx_keccak);
-	sph_keccak512(&ctx_keccak, (const void*) hash, 64);
-	sph_keccak512_close(&ctx_keccak, hash);
+	sph_keccak512(&ctx_keccak, hashA, 64);
+	sph_keccak512_close(&ctx_keccak, hashB);
 
 	sph_luffa512_init(&ctx_luffa);
-	sph_luffa512(&ctx_luffa, (const void*) hash, 64);
-	sph_luffa512_close (&ctx_luffa, hash);
+	sph_luffa512(&ctx_luffa, hashB, 64);
+	sph_luffa512_close(&ctx_luffa, hashA);
 
 	sph_cubehash512_init(&ctx_cubehash);
-	sph_cubehash512(&ctx_cubehash, (const void*) hash, 64);
-	sph_cubehash512_close(&ctx_cubehash, hash);
+	sph_cubehash512(&ctx_cubehash, hashA, 64);
+	sph_cubehash512_close(&ctx_cubehash, hashB);
 
 	sph_shavite512_init(&ctx_shavite);
-	sph_shavite512(&ctx_shavite, (const void*) hash, 64);
-	sph_shavite512_close(&ctx_shavite, hash);
+	sph_shavite512(&ctx_shavite, hashB, 64);
+	sph_shavite512_close(&ctx_shavite, hashA);
 
 	sph_simd512_init(&ctx_simd);
-	sph_simd512(&ctx_simd, (const void*) hash, 64);
-	sph_simd512_close(&ctx_simd, hash);
+	sph_simd512(&ctx_simd, hashA, 64);
+	sph_simd512_close(&ctx_simd, hashB);
 
 	sph_echo512_init(&ctx_echo);
-	sph_echo512(&ctx_echo, (const void*) hash, 64);
-	sph_echo512_close(&ctx_echo, hash);
+	sph_echo512(&ctx_echo, hashB, 64);
+	sph_echo512_close(&ctx_echo, hashA);
 
 	sph_hamsi512_init(&ctx_hamsi);
-	sph_hamsi512(&ctx_hamsi, (const void*) hash, 64);
-	sph_hamsi512_close(&ctx_hamsi, hash);
+	sph_hamsi512(&ctx_hamsi, hashA, 64);
+	sph_hamsi512_close(&ctx_hamsi, hashA);
 
 	sph_fugue512_init(&ctx_fugue);
-	sph_fugue512(&ctx_fugue, (const void*) hash, 64);
-	sph_fugue512_close(&ctx_fugue, hash);
+	sph_fugue512(&ctx_fugue, hashA, 64);
+	sph_fugue512_close(&ctx_fugue, hashA);
 
 	sph_shabal512_init(&ctx_shabal);
-	sph_shabal512(&ctx_shabal, (const void*) hash, 64);
-	sph_shabal512_close(&ctx_shabal, hash);
+	sph_shabal512(&ctx_shabal, hashA, 64);
+	sph_shabal512_close(&ctx_shabal, hashB);
 
 	sph_whirlpool_init(&ctx_whirlpool);
-	sph_whirlpool (&ctx_whirlpool, (const void*) hash, 64);
-	sph_whirlpool_close(&ctx_whirlpool, hash);
+	sph_whirlpool(&ctx_whirlpool, hashB, 64);
+	sph_whirlpool_close(&ctx_whirlpool, hashA);
 
-	sph_sha512_init(&ctx_sha512);
-	sph_sha512(&ctx_sha512,(const void*) hash, 64);
-	sph_sha512_close(&ctx_sha512,(void*) hash);
+	sph_sha512_init(&ctx_sha2);
+	sph_sha512(&ctx_sha2, hashA, 64);
+	sph_sha512_close(&ctx_sha2, hashB);
 
 	sph_haval256_5_init(&ctx_haval);
-	sph_haval256_5(&ctx_haval,(const void*) hash, 64);
-	sph_haval256_5_close(&ctx_haval,hash);
+	sph_haval256_5(&ctx_haval, hashB, 64);
+	sph_haval256_5_close(&ctx_haval, hashA);
 
-	memcpy(output, hash, 32);
+	sph_gost512_init(&ctx_gost);
+	sph_gost512(&ctx_gost, hashA, 64);
+	sph_gost512_close(&ctx_gost, hashB);
+
+	memcpy(output, hashB, 32);
 }
 
 static bool init[MAX_GPUS] = { 0 };
@@ -166,7 +173,7 @@ extern "C" int scanhash_x17(int thr_id, struct work* work, uint32_t max_nonce, u
 	//if (init[thr_id]) throughput = min(throughput, max_nonce - first_nonce);
 
 	if (opt_benchmark)
-		((uint32_t*)ptarget)[7] = 0x00ff;
+		((uint32_t*)ptarget)[7] = 0x0cff;
 
 	if (!init[thr_id])
 	{
@@ -200,14 +207,15 @@ extern "C" int scanhash_x17(int thr_id, struct work* work, uint32_t max_nonce, u
 		x17_haval256_cpu_init(thr_id, throughput);
 
 		CUDA_CALL_OR_RET_X(cudaMalloc(&d_hash[thr_id], 16 * sizeof(uint32_t) * throughput), 0);
+		CUDA_CALL_OR_RET_X(cudaMalloc(&d2_hash[thr_id], 16 * sizeof(uint32_t) * throughput), 0);
 
 		cuda_check_cpu_init(thr_id, throughput);
 
 		init[thr_id] = true;
 	}
 
-	uint32_t _ALIGN(64) endiandata[20];
-	for (int k=0; k < 20; k++)
+	uint32_t _ALIGN(128) endiandata[20];
+	for (int k=0; k < 19; k++)
 		be32enc(&endiandata[k], pdata[k]);
 
 	quark_blake512_cpu_setBlock_80(thr_id, endiandata);
@@ -237,26 +245,30 @@ extern "C" int scanhash_x17(int thr_id, struct work* work, uint32_t max_nonce, u
 		x13_fugue512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 		x14_shabal512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 		x15_whirlpool_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
-		x17_sha512_cpu_hash_64(thr_id, throughput, pdata[19], d_hash[thr_id]); order++;
-		x17_haval256_cpu_hash_64(thr_id, throughput, pdata[19], d_hash[thr_id], 256); order++;
-
+		x17_sha512_cpu_hash_64(thr_id, throughput, pdata[19], d_hash[thr_id], d2_hash[thr_id]); order++;
+		x17_haval256_cpu_hash_64(thr_id, throughput, pdata[19], d_hash[thr_id], 256, d2_hash[thr_id]); order++;
+		
+		streebog_cpu_hash_64(thr_id, throughput, d_hash[thr_id]);
+		
+	
 		*hashes_done = pdata[19] - first_nonce + throughput;
 
 		work->nonces[0] = cuda_check_hash(thr_id, throughput, pdata[19], d_hash[thr_id]);
 		if (work->nonces[0] != UINT32_MAX)
 		{
 			const uint32_t Htarg = ptarget[7];
-			uint32_t _ALIGN(64) vhash[8];
+			uint32_t _ALIGN(128) vhash[8];
 			be32enc(&endiandata[19], work->nonces[0]);
-			x17hash(vhash, endiandata);
+			x18hash(vhash, endiandata);
 
-			if (vhash[7] <= Htarg && fulltest(vhash, ptarget)) {
+			if (vhash[7] < Htarg && fulltest(vhash, ptarget)) {
+		
 				work->valid_nonces = 1;
 				work->nonces[1] = cuda_check_hash_suppl(thr_id, throughput, pdata[19], d_hash[thr_id], 1);
 				work_set_target_ratio(work, vhash);
 				if (work->nonces[1] != 0) {
 					be32enc(&endiandata[19], work->nonces[1]);
-					x17hash(vhash, endiandata);
+					x18hash(vhash, endiandata);
 					bn_set_target_ratio(work, vhash, 1);
 					work->valid_nonces++;
 					pdata[19] = max(work->nonces[0], work->nonces[1]) + 1;
@@ -302,6 +314,7 @@ extern "C" void free_x17(int thr_id)
 	cudaThreadSynchronize();
 
 	cudaFree(d_hash[thr_id]);
+	cudaFree(d2_hash[thr_id]);
 
 	quark_blake512_cpu_free(thr_id);
 	quark_groestl512_cpu_free(thr_id);
